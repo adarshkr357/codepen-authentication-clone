5981717686d99e4b8bcaaf83aec40692cb91fa10
Adarsh Kumar <adarshkr357@gmail.com>
2024-04-14 19:31:58 +0530

    Reverting back

---
+++ functions.py
@@ -0,0 +1,73 @@
+from pymongo import MongoClient
+from requests import post
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+import hashes
+import base64
+
+client = MongoClient(
+    "mongodb+srv://pranavkandpal05:OneaNTBR8XqnROSr@cluster0.wupcbdl.mongodb.net/Cluster0"
+)
+db = client.Codepen
+collection = db.Users
+
+
+def insert_user():
+    login_data = {
+        "username": "pranva",
+        "email": "knakns",
+        "password": "ds122",
+        "last_log": "1/05/2024T1713094382",
+    }
+
+    result = collection.insert_one(login_data)
+    print(f"inserted document ID {result.inserted_id}")
+
+
+def search(query):
+
+    results = collection.find_one(query)
+
+    if results == None:
+        return None
+
+    return results
+
+
+def check_captcha(captcha_response, ip):
+
+    url = "https://www.google.com/recaptcha/api/siteverify"
+
+    payload = {
+        "secret": "6LfIn7opAAAAAHSbjXa8UqDAgw7Ezb1QvKssWDNZ",
+        "response": captcha_response,
+        "remoteip": ip,
+    }
+    req = post(url, payload).json()
+
+    if req.success:
+        return True
+    else:
+        return False
+
+
+def decryptWithPrivateKey(privateKeyPath, encryptedMessage):
+    with open(privateKeyPath, "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(), password=None, backend=default_backend()
+        )
+
+    original_message = private_key.decrypt(
+        base64.b64decode(encryptedMessage),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None,
+        ),
+    )
+
+    return original_message.decode("utf-8")
+
+
+print(decryptWithPrivateKey("./private_key.pem", "..."))
---
+++ server.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request
+from functions import *
+import re
+
+app = Flask(__name__)
+
+
+# user routes:
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/forgot_password")
+def forgot():
+    return render_template("forgot.html")
+
+
+# Backend routes:
+
+
+@app.route("/login-check", methods=["POST"])
+def get_user(user_detail, password, POW, captcha):
+
+    if len(captcha) < 10:
+        return {"success": False, "message": "Captcha is missing"}
+
+    ip_address = request.remote_addr
+
+    if not check_captcha(captcha, ip_address):
+        return {"success": False, "message": "Captcha is invaild"}
+
+    if "@" in user_detail:
+        query = {"email": f"{user_detail}"}
+    else:
+        query = {"username": f"{user_detail}"}
+
+    exist = search(query)
+
+    if exist == None:
+        return {"success": False, "message": "username/email doesn't match"}
+
+    if exist.password != password:
+        return {"success": False, "message": "password doesn't match"}
+
+    return {"success": True, "message": "login successfull"}
+
+
+@app.route("/check-userinfo")
+def check(email, username):
+    if len(email) > 10:
+        email_pattern = r"^[a-zA-Z0-9]+[\._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$"
+
+        if not re.match(email_pattern, email):
+            return {"success": False, "message": "invalid email"}
+
+        query = {"email": f"{email}"}
+        exist = search(query)
+
+        if exist != None:
+            return {"success": False, "message": "email already exists"}
+
+    if len(username) > 2:
+        usr_pattern = r"^[a-zA-Z][a-zA-Z0-9]*(_[a-zA-Z0-9]+)?$"
+
+        if not re.match(usr_pattern, username):
+            return {"success": False, "message": "invalid username"}
+
+        query = {"username": f"{username}"}
+        exist = search(query)
+
+        if exist != None:
+            return {"success": False, "message": "Username already exists"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
